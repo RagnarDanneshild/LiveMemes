5bbc0e04094efffe03f9c0e599f6d6ae719f3f7b
RagnarDanneshild <paveltumash@yandex.ru>
2016-07-03 19:54:11 +0300

    Add memes form And memew view model

---
+++ LiveMemes/Models/MemesViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LiveMemes.Models
+{
+    public class MemesViewModel
+    {
+        public string UpperText { get; set; }
+        public string LowerText { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
